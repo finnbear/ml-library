d20946865a85725072fe5c87e34a096791514dde
finnbear <finnbearone@gmail.com>
2017-05-19 21:36:20 -0700

    CNN compiles and runs.

--- image-cnn-classifier/main.py
+++ image-cnn-classifier/main.py
@@ -6,6 +6,8 @@ from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 import numpy
+from keras import backend as K
+K.set_image_dim_ordering('th')
 
 img_size = 150
 batch_size = 16
@@ -21,17 +23,23 @@ validation_generator = datagen.flow_from_directory('data/validation', target_siz
 
 model = Sequential()
 
-def conv_layer():
-	model.add(Convolution2D(32, (3, 3), input_shape=(img_size, img_size, 3), data_format='channels_first'))
+def conv_layer(first=False):
+	if first:
+		#model.add(ZeroPadding2D(padding=(1, 1)))
+		model.add(Convolution2D(64, (3, 3), input_shape=(3, img_size, img_size), data_format='channels_first'))
+	else:
+		model.add(Convolution2D(64, (3, 3), data_format='channels_first'))
 	model.add(Activation('relu'))
 	model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
 
-conv_layer()
+conv_layer(first=True)
 conv_layer()
 conv_layer()
 
+print model.output_shape
+
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(64, input_shape=(batch_size, 3)))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
@@ -39,10 +47,10 @@ model.add(Activation('sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-model.fit_generator(training_generator, epochs=epochs, samples_per_epoch=samples_per_epoch, validation_data=validation_generator, nb_val_samples=832)
+model.fit_generator(training_generator, epochs=epochs, steps_per_epoch=samples_per_epoch, validation_data=validation_generator, validation_steps=100)
 
-model.save_weights('models/')
+model.save_weights('models/cnn.h5')
 
-img = image.load_image('test/img001.jpg', target_size=(224, 224))
+img = image.load_image('data/validation/cats/981.jpg', target_size=(224, 224))
 prediction = model.predict(img)
 print prediction
